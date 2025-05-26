19f29794e71b3d322f7c502d9603f555f120710f
Harjyot Gulati <harjyotsinghgulati@gmail.com>
2025-05-26 13:17:41 +0530

    how to display todo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
+import { nanoid } from 'nanoid'
 import React, { useState } from 'react'
 
 const App = () => {
 
   const [todo,setTodo] = useState([{id:1,title:"kam karle bhai",isCompleted:false},])
   const [title,setTitle] = useState("")
-  const [completed,setCompleted] = useState(false);
-  const [gender,setGender] = useState("female")
+
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    const newTodo = {
+      id:nanoid(),
+      title:title,
+      isCompleted:false,
+    }
+    setTodo([...todo,newTodo]);
+    setTitle("");
+  }
 
   return (
     <div>
       <h1>Create Task.</h1>  
-      <form>
 
-        <input type="text" placeholder='title' onChange={(e)=>setTitle(e.target.value)}/>
+      <form onSubmit={submitHandler}>
+        <input type="text" value={title} placeholder='title' onChange={(e)=>setTitle(e.target.value)}/>
         <br />
-
-        <br />
-        <input checked={completed} onChange={(e)=>setCompleted(e.target.checked)} type="checkbox"/> Completed
-        <br /><br />
-
-        <input checked={gender == "male"&&true} type='radio' onChange={(e)=>setGender(e.target.value)} /> Male
-        <input checked={gender == "female"&&true} type="radio" onChange={(e)=>setGender(e.target.value)} /> Female
-        <br />
-
         <br />
         <button>Create Todo.</button>
       </form>
+      <h1>All Todo</h1>
+        <ol>
+          {
+            todo.map((todo)=>{
+              return <li key={todo.id}>{todo.title}</li>
+            })
+          }
+        </ol>    
     </div>
   )
 }
